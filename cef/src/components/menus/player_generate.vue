d0628c1ca997f8fdb7c4f04f44d72d6248a7f061
umudik <seyitumutbicer@gmail.com>
2022-06-06 04:36:25 +0300

    dev

--- cef/src/components/menus/player_generate.vue
+++ cef/src/components/menus/player_generate.vue
@@ -9,6 +9,7 @@ v-dialog(width="600")
       v-card-text 
         v-text-field(label="Name" )
         v-text-field(label="Age"  type="number" min="18" max="99")
+        v-slider(:label="item" v-for ="item of one_zero_keys" min="-1" max="1" thumb-label step="0.01")
       v-card-actions
         v-spacer
         v-btn(color = "green" dark)
@@ -17,13 +18,62 @@ v-dialog(width="600")
 </template>
 
 <script>
+import lodash from "lodash";
 export default {
   data() {
-    return {};
+    return {
+      player_cloth: {
+        cloth_head: { type: "object" },
+        cloth_beard: { type: "object" },
+        cloth_hair: { type: "object" },
+        cloth_torso: { type: "object" },
+        cloth_legs: { type: "object" },
+        cloth_hand: { type: "object" },
+        cloth_foot: { type: "object" },
+        cloth_eye: { type: "object" },
+        cloth_accessory1: { type: "object" },
+        cloth_accessory2: { type: "object" },
+        cloth_decal: { type: "object" },
+        cloth_auxiliary: { type: "object" },
+      },
+      player_zero_one_keys: {
+        customization_gender: { type: "boolean" },
+        customization_mother_blend: { type: "number" },
+        customization_father_blend: { type: "number" },
+        customization_f_blend_shape: { type: "number" },
+        customization_f_blend_skin: { type: "number" },
+        customization_hair_highlight: { type: "number" },
+        customization_hair_color: { type: "number" },
+        customization_nose_width: { type: "number" },
+        customization_nose_height: { type: "number" },
+        customization_nose_length: { type: "number" },
+        customization_nose_bridge: { type: "number" },
+        customization_nose_tip: { type: "number" },
+        customization_nose_bridge_shift: { type: "number" },
+        customization_brow_height: { type: "number" },
+        customization_brow_width: { type: "number" },
+        customization_c_bone_height: { type: "number" },
+        customization_c_bone_width: { type: "number" },
+        customization_cheek_width: { type: "number" },
+        customization_eyes: { type: "number" },
+        customization_lips: { type: "number" },
+        customization_jaw_width: { type: "number" },
+        customization_jaw_height: { type: "number" },
+        customization_chin_length: { type: "number" },
+        customization_chin_pos: { type: "number" },
+        customization_chin_width: { type: "number" },
+        customization_chin_shape: { type: "number" },
+        customization_neck_width: { type: "number" },
+      },
+    };
   },
   mounted() {},
   methods: {},
-  computed: {},
+  computed: {
+    one_zero_keys() {
+      return lodash.keys(this.player_zero_one_keys);
+    },
+  },
 };
 </script>
 
