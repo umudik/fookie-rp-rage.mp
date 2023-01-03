b3603ef9084635e31f8f4a0f62e748f4d38a0b15
umudik <seyitumutbicer@gmail.com>
2023-01-04 00:40:21 +0300

    dev

--- server/packages/api/inventory/models/item.js
+++ server/packages/api/inventory/models/item.js
@@ -28,7 +28,7 @@ module.exports = {
     lifecycle: {
         create: {
             modify: ["set_inventory_and_type", "slot_fixer"],
-            rule: ["check_weight", "openable", "has_slot", "is_slot_avaible", "check_item_amount"],
+            rule: ["check_weight", "openable", "has_slot", "is_slot_avaible"],
             role: ["system"],
             effect: ["organise_inventory", "do_item_type_events"],
         },
@@ -37,7 +37,7 @@ module.exports = {
         },
         update: {
             modify: ["find_items"],
-            rule: ["check_weight", "openable", "has_slot", "is_slot_avaible", "check_item_amount"],
+            rule: ["check_weight", "openable", "has_slot", "is_slot_avaible"],
             role: ["system"],
             effect: [],
         },
--- server/packages/api/inventory/rules/is_slot_avaible.js
+++ server/packages/api/inventory/rules/is_slot_avaible.js
@@ -3,20 +3,21 @@ module.exports = async function (ctx) {
         name: "is_slot_avaible",
         function: async function (payload, ctx, state) {
             if (payload.method === "create") {
-                let res = await ctx.run({
-                    token: process.env.SYSTEM_TOKEN,
-                    model: "item",
-                    method: "count",
-                    query: {
-                        filter: {
-                            inventory: payload.body.inventory,
-                            slot: payload.body.slot,
-                        }
+                const emptySlots = await ctx.helpers.emptySlots(payload.body.inventory)
+                if (emptySlots.length == 0) {
+                    return false
+                }
+                if (payload.body.slot) {
+                    if (!lodash.includes(emptySlots, payload.body.slot)) {
+                        payload.body.slot = emptySlots[0]
                     }
-                })
-                return res.data == 0
+                }
+
+                const needed_slot_size = Math.celil(payload.body.amount / state.item_type.stack)
+                return needed_slot_size <= emptySlots.length
             }
             if (payload.method === "update") {
+                const inventories = state.items.map(i => i.inventory)
                 const res = await ctx.run({
                     token: process.env.SYSTEM_TOKEN,
                     model: "item",
